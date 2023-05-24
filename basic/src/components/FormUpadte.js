82da39fea1d4679e5c6c66a602f51d7ec8a6e521
TAN DANG <88600710+dangvomytan@users.noreply.github.com>
2023-05-24 12:08:54 +0700

    Hackthon 3 basic

--- basic/src/components/FormUpadte.js
+++ basic/src/components/FormUpadte.js
@@ -15,47 +15,49 @@ function FormUpadate(props) {
     // Truyền dữ liệu cho app
     props.handleUpdateForm({ ...formData });
 
-     props.handleClose();
+    props.handleClose();
   };
   console.log(formData);
   return (
-     <>     <Modal
-     show={props.show}
-     onHide={props.handleClose}
-     backdrop="static"
-     keyboard={false}
-   >
-      <Modal.Dialog>
-        <Form onSubmit={handleSubmitForm}>
-          <Modal.Body>
-            <Form.Control type="text" name="id"
-          onChange={handleOnChange}
-             hidden
-              />
-            <Form.Control
-              className="title_form"
-              type="text"
-              placeholder="Title"
-              style={{ border: "none" }}
-              name="title"
-              onChange={handleOnChange}
-            />
-            <Form.Control
-              className="title_form"
-              as="textarea"
-              placeholder="Take a note ..."
-              style={{ height: "100px", border: "none" }}
-              name="content"
-              onChange={handleOnChange}
-            />
-            <Button variant="warning" id="btn_add" type="submit">
-              +
-            </Button>
-          </Modal.Body>
-        </Form>
-      </Modal.Dialog>
-    </Modal></>
 
+
+      <Modal
+        show={props.show}
+        onHide={props.handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+
+          <Form onSubmit={handleSubmitForm}>
+            <Modal.Body>
+              <Form.Control
+                type="text"
+                name="id"
+                onChange={handleOnChange}
+                hidden
+              />
+              <Form.Control
+                className="title_form"
+                type="text"
+                placeholder="Title"
+                style={{ border: "none" }}
+                name="title"
+                onChange={handleOnChange}
+              />
+              <Form.Control
+                className="title_form"
+                as="textarea"
+                placeholder="Take a note ..."
+                style={{ height: "100px", border: "none" }}
+                name="content"
+                onChange={handleOnChange}
+              />
+              <Button variant="warning" id="btn_add" type="submit">
+                +
+              </Button>
+            </Modal.Body>
+          </Form>
+      </Modal>
   );
 }
 export default FormUpadate;
